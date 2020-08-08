72fd0f7a4bd46e293d1dc8402eec31031500658c
kain <zhangtqx@vip.qq.com>
2020-08-08 09:41:39 +0800

    更新单元测试

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,49 +1,55 @@
-import {en_US, zh_CN} from 'ng-zorro-antd';
+import { en_US, zh_CN } from 'ng-zorro-antd';
+import { factoryBitConfig } from 'ngx-bit/operates';
 
-export const environment = {
-  production: true,
-  bit: {
-    originUrl: 'https://api.kainonly.com',
-    staticUrl: 'https://cdn.kainonly.com/',
-    iconUrl: 'https://cdn.kainonly.com/',
+const bit = factoryBitConfig({
+  url: {
+    api: 'https://api.kainonly.com',
+    static: 'https://cdn.kainonly.com/',
+    icon: 'https://cdn.kainonly.com/'
+  },
+  api: {
     namespace: '/system',
-    uploadsUrl: false,
-    uploadsPath: 'system/main/uploads',
-    withCredentials: true,
-    httpInterceptor: true,
-    pageLimit: 10,
-    breadcrumbTop: 0,
-    col: {
-      label: {
-        nzXXl: 4,
-        nzXl: 5,
-        nzLg: 6,
-        nzMd: 7,
-        nzSm: 24
-      },
-      control: {
-        nzXXl: 8,
-        nzXl: 9,
-        nzLg: 10,
-        nzMd: 14,
-        nzSm: 24,
-      },
-      submit: {
-        nzXXl: {span: 8, offset: 4},
-        nzXl: {span: 9, offset: 5},
-        nzLg: {span: 10, offset: 6},
-        nzMd: {span: 14, offset: 6},
-        nzSm: {span: 24, offset: 0}
-      }
+    upload: '/system/main/uploads',
+    withCredentials: true
+  },
+  col: {
+    label: {
+      nzXXl: 4,
+      nzXl: 5,
+      nzLg: 6,
+      nzMd: 7,
+      nzSm: 24
     },
-    localDefault: 'zh_cn',
-    localeBind: new Map([
+    control: {
+      nzXXl: 8,
+      nzXl: 9,
+      nzLg: 10,
+      nzMd: 14,
+      nzSm: 24
+    },
+    submit: {
+      nzXXl: { span: 8, offset: 4 },
+      nzXl: { span: 9, offset: 5 },
+      nzLg: { span: 10, offset: 6 },
+      nzMd: { span: 14, offset: 6 },
+      nzSm: { span: 24, offset: 0 }
+    }
+  },
+  locale: {
+    default: 'zh_cn',
+    mapping: new Map<number, string>([
+      [0, 'zh_cn'],
+      [1, 'en_us']
+    ]),
+    bind: new Map<string, any>([
       ['zh_cn', zh_CN],
       ['en_us', en_US]
-    ]),
-    i18nDefault: 'zh_cn',
-    i18nContain: ['zh_cn', 'en_us'],
-    i18nSwitch: [
+    ])
+  },
+  i18n: {
+    default: 'zh_cn',
+    contain: ['zh_cn', 'en_us'],
+    switch: [
       {
         i18n: 'zh_cn',
         name: {
@@ -59,5 +65,11 @@ export const environment = {
         }
       }
     ]
-  }
+  },
+  page: 20
+});
+
+export const environment = {
+  production: true,
+  bit
 };
