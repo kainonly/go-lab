4b67d0f2b9d71a4d33de62bd92eb0a0d61bc6e9c
kain <zhangtqx@vip.qq.com>
2020-11-05 18:48:18 +0800

    update

---
+++ mvc/mvc.go
@@ -0,0 +1,42 @@
+package mvc
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/huandu/xstrings"
+	"reflect"
+)
+
+type Mvc struct {
+	*gin.Engine
+	dependency interface{}
+}
+
+func (c *Mvc) Dependency(dependency interface{}) {
+	c.dependency = dependency
+}
+
+func (c *Mvc) Handle(handlersFn interface{}) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		switch method := handlersFn.(type) {
+		case func() interface{}:
+			ctx.JSON(200, method())
+			break
+		case func(ctx *gin.Context) interface{}:
+			ctx.JSON(200, method(ctx))
+			break
+		case func(*gin.Context, interface{}) interface{}:
+			ctx.JSON(200, method(ctx, c.dependency))
+			break
+		}
+	}
+}
+
+func (c *Mvc) AutoController(path string, controller interface{}) {
+	typ := reflect.TypeOf(controller)
+	val := reflect.ValueOf(controller)
+	for i := 0; i < typ.NumMethod(); i++ {
+		name := typ.Method(i).Name
+		method := val.MethodByName(name).Interface()
+		c.POST(path+"/"+xstrings.ToSnakeCase(name), c.Handle(method))
+	}
+}
