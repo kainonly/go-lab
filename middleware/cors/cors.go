a7cb89c028af6fcaecd408245c66362aa2409830
kain <zhangtqx@vip.qq.com>
2020-11-06 23:13:31 +0800

    update

---
+++ middleware/cors/cors.go
@@ -0,0 +1,40 @@
+package cors
+
+import (
+	"github.com/gin-gonic/gin"
+	"strconv"
+	"strings"
+)
+
+type Option struct {
+	Origin        []string
+	Method        []string
+	AllowHeader   []string
+	ExposedHeader []string
+	MaxAge        int
+	Credentials   bool
+}
+
+func Cors(option Option) gin.HandlerFunc {
+	origin := strings.Join(option.Origin, ",")
+	method := strings.Join(option.Method, ",")
+	allowHeader := strings.Join(option.AllowHeader, ",")
+	exposedHeader := strings.Join(option.ExposedHeader, ",")
+	maxAge := strconv.Itoa(option.MaxAge)
+
+	return func(ctx *gin.Context) {
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.Status(200)
+			return
+		}
+		ctx.Header("access-control-allow-origin", origin)
+		ctx.Header("Access-Control-Allow-Methods", method)
+		ctx.Header("Access-Control-Allow-Headers", allowHeader)
+		ctx.Header("Access-Control-Expose-Headers", exposedHeader)
+		ctx.Header("Access-Control-Max-Age", maxAge)
+		if option.Credentials {
+			ctx.Header("Access-Control-Allow-Credentials", "true")
+		}
+		ctx.Next()
+	}
+}
