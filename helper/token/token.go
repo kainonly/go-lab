dafeb4b6f76ac68969c582cf6d393be81a88d0aa
kain <zhangtqx@vip.qq.com>
2020-11-23 23:05:15 +0800

    update

--- helper/token/token.go
+++ helper/token/token.go
@@ -8,9 +8,8 @@ import (
 )
 
 var (
-	Key     []byte
-	Options map[string]Option
-	Method  jwt.SigningMethod = jwt.SigningMethodHS256
+	Key    []byte
+	Method jwt.SigningMethod = jwt.SigningMethodHS256
 )
 
 type Token struct {
@@ -26,57 +25,48 @@ func (c *Token) Claims() jwt.MapClaims {
 	return c.claims
 }
 
-type Option struct {
-	Issuer   string   `yaml:"issuer"`
-	Audience []string `yaml:"audience"`
-	Expires  uint     `yaml:"expires"`
-}
-
-type Handle func(option Option) (claims jwt.MapClaims, err error)
-
-func Make(scene string, claims jwt.MapClaims) (token *Token, err error) {
-	option, exists := Options[scene]
-	if !exists {
-		err = fmt.Errorf("the [%v] scene does not exist", scene)
-		return
-	}
+// create a token
+// @param claims jwt.MapClaims @refer http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#Claims
+// @param expires time.Duration
+// @return token *Token
+func Make(claims jwt.MapClaims, expires time.Duration) (token *Token, err error) {
 	token = new(Token)
 	claims["jti"] = uuid.New()
 	claims["iat"] = time.Now().Unix()
-	claims["iss"] = option.Issuer
-	claims["aud"] = option.Audience
-	claims["exp"] = time.Now().Add(time.Second * time.Duration(option.Expires)).Unix()
-	t := jwt.NewWithClaims(Method, claims)
-	token.claims = t.Claims.(jwt.MapClaims)
-	token.value, err = t.SignedString(Key)
-	if err != nil {
+	claims["exp"] = time.Now().Add(expires).Unix()
+	ref := jwt.NewWithClaims(Method, claims)
+	token.claims = ref.Claims.(jwt.MapClaims)
+	if token.value, err = ref.SignedString(Key); err != nil {
 		return
 	}
-
 	return
 }
 
-func Verify(scene string, tokenString string, refresh Handle) (claims jwt.MapClaims, err error) {
-	option, exists := Options[scene]
-	if !exists {
-		err = fmt.Errorf("the [%v] scene does not exist", scene)
-		return
-	}
+// token refresh logic
+// @param claims jwt.MapClaims
+// @return jwt.MapClaims
+type RefreshHandle func(claims jwt.MapClaims) (jwt.MapClaims, error)
+
+// verify that the token is valid
+// @param tokenString string
+// @param refresh RefreshHandle
+// @return claims jwt.MapClaims
+func Verify(tokenString string, refresh RefreshHandle) (claims jwt.MapClaims, err error) {
 	var token *jwt.Token
-	token, err = jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+	if token, err = jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return Key, nil
-	})
-	if err != nil {
+	}); err != nil {
 		if ve, ok := err.(*jwt.ValidationError); ok {
 			if ve.Errors == jwt.ValidationErrorExpired {
-				return refresh(option)
+				if token != nil {
+					return refresh(token.Claims.(jwt.MapClaims))
+				}
 			}
 		}
-	} else {
-		claims = token.Claims.(jwt.MapClaims)
+		return
 	}
-	return
+	return token.Claims.(jwt.MapClaims), nil
 }
--- helper/token/token_test.go
+++ helper/token/token_test.go
@@ -2,36 +2,27 @@ package token
 
 import (
 	"github.com/dgrijalva/jwt-go"
-	"os"
 	"testing"
+	"time"
 )
 
 var token *Token
 var err error
 
-func TestMain(m *testing.M) {
-	Options = map[string]Option{
-		"system": {
-			Issuer:   "helper",
-			Audience: []string{"dev"},
-			Expires:  3600,
-		},
-	}
-	os.Exit(m.Run())
-}
-
 func TestMake(t *testing.T) {
 	claims := jwt.MapClaims{
+		"iss":      "gin-extra",
+		"aud":      []string{"tests"},
 		"username": "kain",
 	}
-	if token, err = Make("system", claims); err != nil {
+	if token, err = Make(claims, time.Hour*2); err != nil {
 		t.Fatal(err)
 	}
 	t.Log(token.String())
 }
 
 func TestVerify(t *testing.T) {
-	claims, err := Verify("system", token.String(), nil)
+	claims, err := Verify(token.String(), nil)
 	if err != nil {
 		t.Fatal(err)
 	}
