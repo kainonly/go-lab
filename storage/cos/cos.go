3f072c31272a8ac89c34cfa1914e5aa77ac182d3
kain <zhangtqx@vip.qq.com>
2021-02-25 15:08:22 +0800

    update

--- storage/cos/cos.go
+++ storage/cos/cos.go
@@ -1,9 +1,18 @@
 package cos
 
 import (
+	"context"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/kainonly/gin-extra/str"
 	"github.com/tencentyun/cos-go-sdk-v5"
+	"mime/multipart"
 	"net/http"
 	"net/url"
+	"strconv"
+	"time"
 )
 
 var (
@@ -28,14 +37,75 @@ func Client() *cos.Client {
 	})
 }
 
-func Put(key string) {
-
+func Put(fileHeader *multipart.FileHeader) (fileName string, err error) {
+	client := Client()
+	fileName = time.Now().Format("2006-01-02") + "/" +
+		str.Uuid().String()
+	var file multipart.File
+	if file, err = fileHeader.Open(); err != nil {
+		return
+	}
+	if _, err = client.Object.Put(
+		context.Background(), fileName, file, nil,
+	); err != nil {
+		return
+	}
+	return
 }
 
-func Delete() {
-
+func Delete(keys []string) error {
+	client := Client()
+	objects := make([]cos.Object, len(keys))
+	for index, value := range keys {
+		objects[index] = cos.Object{
+			Key: value,
+		}
+	}
+	if _, _, err := client.Object.DeleteMulti(context.Background(), &cos.ObjectDeleteMultiOptions{
+		Quiet:   true,
+		Objects: objects,
+	}); err != nil {
+		return err
+	}
+	return nil
 }
 
-func GeneratePostPresigned() {
+func GeneratePostPresigned(expired int64, conditions ...[]interface{}) (data map[string]interface{}, err error) {
+	now := time.Now()
+	keyTime := strconv.Itoa(int(now.Unix())) + ";" + strconv.Itoa(int(now.Unix()+expired))
+	fileName := time.Now().Format("2006-01-02") + "/" + str.Uuid().String()
+	conditions = append(conditions, []interface{}{"bucket", DEF.Bucket})
+	conditions = append(conditions, []interface{}{"starts-with", "$key", fileName})
+	conditions = append(conditions, []interface{}{"q-sign-algorithm", "sha1"})
+	conditions = append(conditions, []interface{}{"q-ak", DEF.SecretID})
+	conditions = append(conditions, []interface{}{"q-sign-time", keyTime})
+	policy := map[string]interface{}{
+		"expiration": now.Add(time.Duration(expired) * time.Second).UTC().Format(time.RFC3339),
+		"conditions": conditions,
+	}
+	var policyData []byte
+	if policyData, err = jsoniter.Marshal(policy); err != nil {
+		return
+	}
+	signKeyHash := hmac.New(sha1.New, []byte(DEF.SecretKey))
+	signKeyHash.Write([]byte(keyTime))
+	signKey := base64.StdEncoding.EncodeToString(signKeyHash.Sum(nil))
+	stringToSignHash := sha1.New()
+	stringToSignHash.Write(policyData)
+	stringToSign := base64.StdEncoding.EncodeToString(stringToSignHash.Sum(nil))
+	signatureHash := hmac.New(sha1.New, []byte(signKey))
+	signatureHash.Write([]byte(stringToSign))
+	signature := base64.StdEncoding.EncodeToString(signatureHash.Sum(nil))
+	data = map[string]interface{}{
+		"filename": fileName,
+		"option": map[string]interface{}{
+			"ak":             DEF.SecretID,
+			"policy":         base64.StdEncoding.EncodeToString(policyData),
+			"key_time":       keyTime,
+			"sign_algorithm": "sha1",
+			"signature":      signature,
+		},
+	}
+	return
 
 }
