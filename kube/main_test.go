65272d969ee8ef43a2a35d698c68ef4a8c8ef4e1
kain <zhangtqx@vip.qq.com>
2023-05-17 10:43:33 +0800

    update

--- kube/main_test.go
+++ kube/main_test.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"flag"
 	"github.com/stretchr/testify/assert"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	core "k8s.io/api/core/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -36,13 +37,55 @@ func TestMain(m *testing.M) {
 }
 
 func TestNodes(t *testing.T) {
-	data, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	data, err := clientset.CoreV1().Nodes().List(context.TODO(), meta.ListOptions{})
 	assert.NoError(t, err)
 	t.Log(data)
 }
 
 func TestDeployments(t *testing.T) {
-	data, err := clientset.AppsV1().Deployments("kube-system").List(context.TODO(), metav1.ListOptions{})
+	data, err := clientset.AppsV1().Deployments("kube-system").List(context.TODO(), meta.ListOptions{})
 	assert.NoError(t, err)
 	t.Log(data)
+}
+
+func TestConfigMapCreate(t *testing.T) {
+	configMap := &core.ConfigMap{
+		ObjectMeta: meta.ObjectMeta{
+			Namespace: "kube-system",
+			Name:      "test",
+		},
+		Data: map[string]string{
+			"ini": "engine = On",
+		},
+	}
+
+	data, err := clientset.CoreV1().
+		ConfigMaps("kube-system").
+		Create(context.TODO(), configMap, meta.CreateOptions{})
+	assert.NoError(t, err)
+	t.Log(data)
+}
+
+func TestConfigMapUpdate(t *testing.T) {
+	configMap := &core.ConfigMap{
+		ObjectMeta: meta.ObjectMeta{
+			Namespace: "kube-system",
+			Name:      "test",
+		},
+		Data: map[string]string{
+			"ini": "engine = On",
+			"zx":  "cccc",
+		},
+	}
+	data, err := clientset.CoreV1().
+		ConfigMaps("kube-system").Update(context.TODO(), configMap, meta.UpdateOptions{})
+	assert.NoError(t, err)
+	t.Log(data)
+}
+
+func TestConfigMapDelete(t *testing.T) {
+	err := clientset.CoreV1().
+		ConfigMaps("kube-system").
+		Delete(context.TODO(), "test", meta.DeleteOptions{})
+	assert.NoError(t, err)
 }
